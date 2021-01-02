b59ffbec2bdd2a22d34980d903f41b4db81eb9ad
Nick Olsen <nolsen@idiq.com>
2021-01-02 12:38:23 -0800

    refactor matchingParens and add a different version

--- src/automata/pushdown/matchingParens.test.ts
+++ src/automata/pushdown/matchingParens.test.ts
@@ -1,9 +1,17 @@
-import matchingParens from "./matchingParens";
+import matchingParens, { matchingParens2 } from "./matchingParens";
 
 describe('matchingParens', () => {
     it('recognizes strings with matching parentheses', () => {
         expect(matchingParens('(())()')).toBe(true)
         expect(matchingParens('())')).toBe(false)
         expect(matchingParens('((())')).toBe(false)
+    })
+})
+
+describe('matchingParens2', () => {
+    it('recognizes strings with matching parentheses', () => {
+        expect(matchingParens2('(())()')).toBe(true)
+        expect(matchingParens2('())')).toBe(false)
+        expect(matchingParens2('((())')).toBe(false)
     })
 })
--- src/automata/pushdown/matchingParens.ts
+++ src/automata/pushdown/matchingParens.ts
@@ -1,18 +1,54 @@
-function matchingParens(str: string): boolean {
-    const stack = []
+function transition (stack: string[], symbol: string): string[] | void {
+    const _stack = stack.slice()
+    switch (symbol) {
+        case '(':
+            _stack.push(symbol)
+            break
+        case ')':
+            if (!_stack.pop()) {
+                return
+            }
+            break
+    }
+
+    return _stack
+}
+
+export default function matchingParens(str: string): boolean {
+    let stack: string[] = []
 
     for (let i = 0; i < str.length; i++) {
-        if (str[i] === '(') {
-            stack.push(str[i])
-        } else if (str[i] === ')') {
-            const ret = stack.pop()
-            if (!ret) {
-                return false
-            }
+        const newStack = transition(stack, str[i])
+
+        if (!Array.isArray(newStack)) {
+            return false
         }
+
+        stack = newStack
     }
 
     return !stack.length
 }
 
-export default matchingParens
+/**
+ * Use a number instead of an actual stack
+ */
+export function matchingParens2(str: string): boolean {
+    let count = 0
+    for (let i = 0; i < str.length; i++) {
+        switch (str[i]) {
+            case '(':
+                count++
+                break
+            case ')':
+                count--
+                break
+        }
+
+        if (count < 0) {
+            return false
+        }
+    }
+
+    return !count
+}
